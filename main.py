f19d53647e00b8b8f54342cd34033cc5ae084a1a
youssefhanin77 <99509548+youssefhanin77@users.noreply.github.com>
2024-04-26 18:50:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import asyncio
 from pytube import Playlist
 
 
-TOKEN = '' # Bot Token
-GUILD_ID = '' # Server id
-CHANNEL_ID = '' # channel id ( Voice / Stage )
+TOKEN = ENV.TOKEN # Bot Token
+GUILD_ID = '1157418594649522268' # Server id
+CHANNEL_ID = '1229892286691938365' # channel id ( Voice / Stage )
 
 intents = discord.Intents.default()
 intents.messages = True
